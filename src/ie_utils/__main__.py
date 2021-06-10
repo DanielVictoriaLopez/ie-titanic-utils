7ccdc2940a749c0bb877b7cb6c11ae4687f642e6
Daniel Victoria <daniel.victoria@student.ie.edu>
2021-06-10 09:11:56 +0200

    Fixed errors and add parser

--- src/ie_utils/__main__.py
+++ src/ie_utils/__main__.py
@@ -2,11 +2,16 @@ import sys
 
 from ie_utils import tokenize
 
+import argparse
+
+parser = argparse.ArgumentParser(description='IE Tokenizer.')
+parser.add_argument('sentence', metavar='sentence', type=str,
+                    help='Sentence to parse')
+
+args = parser.parse_args().sentence
+
 def main():
-    print(tokenize("Hello world"))
+    print(tokenize(args))
 
 if __name__ == "__main__":
     main()
-
-if str(sys.argv[1]):
-    print(tokenize(str(sys.argv[1])))
