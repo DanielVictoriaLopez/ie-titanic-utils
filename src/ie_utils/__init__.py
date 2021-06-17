a646d3c22dd792bd485e250ea78802ced5843486
Daniel Victoria <daniel.victoria@student.ie.edu>
2021-06-17 14:03:36 +0200

    new functionalities in tokenize and tests

--- src/ie_utils/__init__.py
+++ src/ie_utils/__init__.py
@@ -6,11 +6,28 @@ __version__ = "0.1.0"  # semver.org
 
 import pandas as pd
 
+import re
 
-def tokenize(text,lower=False):
-    if lower:
-        text = text.lower()
-    return text.split()
+
+def tokenize(text,lower=False, remove_stopwords=False, remove_punctuation=False):
+    if text =="":
+        raise ValueError("Cannot tokenize empty sentence")
+    else:
+        if lower:
+            text = text.lower()
+
+        if remove_stopwords:
+            stopwords = ["a", "the", "or","and"]
+            for i in stopwords:
+                text = re.sub(r'\s*\b' + i + r'\b\s*', ' ', text).strip()
+
+        if remove_punctuation:
+            punctuation = [".", ",", "!"]
+            for i in punctuation:
+                text = text.replace(i, '')
+        text = text.split()
+
+        return text
 
 
 if __name__ == "__main__":
--- tests/test_ie_utils.py
+++ tests/test_ie_utils.py
@@ -1,5 +1,9 @@
 import pytest
 
+import pandas as pd
+
+from numpy.testing import assert_array_almost_equal
+
 from ie_utils import tokenize
 
 @pytest.mark.parametrize("sentence, expected_tokens",[
@@ -13,6 +17,31 @@ def test_tokenize_returns_expected_list(sentence, expected_tokens):
     
 def test_tokenize_returns_lowercase_tokens():
     sentence = "This is a sentence"
+
     expected_tokens = ["this","is","a","sentence"]
     
-    tokens = tokenize(sentence, lower=True) 
+    assert expected_tokens == tokenize(sentence, lower=True) 
+    
+def test_series_are_approximately_equal():
+    ser = pd.Series([0.1,0.2,0.1+0.2])
+    expected_ser = pd.Series([0.1,0.2,0.3])
+    
+    assert_array_almost_equal(ser, expected_ser)
+    
+def test_tokenize_returns_tokens_without_stopwords():
+    sentence = "and this is a one sentence or the other"
+
+    expected_tokens = ["this","is","one","sentence","other"]
+    
+    assert expected_tokens == tokenize(sentence, remove_stopwords=True)  
+    
+def test_tokenize_returns_tokens_without_punctuation():
+    sentence = "This, is a sentence. One sentence!"
+
+    expected_tokens = ["This","is","a","sentence","One","sentence"]
+    
+    assert expected_tokens == tokenize(sentence, remove_punctuation=True) 
+    
+def test_ValueError():
+    with pytest.raises(ValueError, match="Cannot tokenize empty sentence"):
+        tokenize("")
